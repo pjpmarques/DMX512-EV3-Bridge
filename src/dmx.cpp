67b591e0cbd3c71911ddc4557a77b5c7250b68d0
Paulo Marques <pjp.marques@gmail.com>
2023-10-05 12:56:17 +0100

    Better use of RGB LED for status & cleanup.

--- src/dmx.cpp
+++ src/dmx.cpp
@@ -1,22 +1,21 @@
 
 #include <Arduino.h>
 #include <HardwareSerial.h>
-#include <dmx.h>
+#include "dmx.h"
 
-DMX::DMX(int uart_number, int rx, int tx, int en) :
-    uart(uart_number), rx_pin(rx), tx_pin(tx), en_pin(en) {
+DMX::DMX(int uart_number, int rx, int tx, int en) : uart(uart_number), rx_pin(rx), tx_pin(tx), en_pin(en)
+{
 }
 
-DMX::DMX() : DMX(DMX_DEFAULT_UART, DMX_RX_PIN, DMX_TX_PIN, DMX_EN_PIN) {
-    uart.setRxBufferSize(DMX_MAX_CHANNEL+1);
-    pinMode(en_pin, OUTPUT);
-    pinMode(rx_pin, INPUT);
-} 
-
-DMX::~DMX() {
+DMX::DMX() : DMX(DMX_DEFAULT_UART, DMX_RX_PIN, DMX_TX_PIN, DMX_EN_PIN)
+{
+  uart.setRxBufferSize(DMX_MAX_CHANNEL + 1);
+  pinMode(en_pin, OUTPUT);
+  pinMode(rx_pin, INPUT);
 }
 
-int DMX::update() {
+int DMX::update()
+{
   unsigned long t0 = millis();
 
   uart.begin(DMX_BAUD, SERIAL_8N2, rx_pin, tx_pin);
@@ -24,26 +23,30 @@ int DMX::update() {
   digitalWrite(en_pin, LOW);
 
   bool good_start = false;
-  while (!good_start) {
+  while (!good_start)
+  {
     // Wait while RX is HIGH (idle)
-    while (digitalRead(rx_pin) == HIGH) 
-        ;
+    while (digitalRead(rx_pin) == HIGH)
+      ;
 
     // We've detected RX became low, start counting time
     unsigned long t1 = micros();
 
     // Now the signal needs to be low for at least 88 usec
-    while (true) {
-        // If the signal becomes high before 88usec, it was a false start, if it continues low after that, it's a good start
-        if (digitalRead(rx_pin) == HIGH) {
-            break;
-        }
+    while (true)
+    {
+      // If the signal becomes high before 88usec, it was a false start, if it continues low after that, it's a good start
+      if (digitalRead(rx_pin) == HIGH)
+      {
+        break;
+      }
 
-        unsigned long t2 = micros();
-        if (t2-t1 >= DMX_START_DELAY) {
-            good_start = true;
-            break;
-        }
+      unsigned long t2 = micros();
+      if (t2 - t1 >= DMX_START_DELAY)
+      {
+        good_start = true;
+        break;
+      }
     }
   }
 
@@ -59,18 +62,16 @@ int DMX::update() {
   uart.end();
 
   // Sanity check. Channel zero always needs to be zero.
-  if (channel[0] != 0x00) {
+  if (channel[0] != 0x00)
+  {
     return -1;
   }
 
   // All ok, returns channels read (which it total bytes read minus start code)
-  return channelsRead-1;
+  return channelsRead - 1;
 }
 
-uint8_t DMX::read(int channel_num) {
-    return channel[channel_num];
-}
-
-void DMX::set_timeout(unsigned milisecconds) {
-  this->timeout = milisecconds;
+uint8_t DMX::read(int channel_num)
+{
+  return channel[channel_num];
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,36 +2,38 @@
 #include <Arduino.h>
 #include <HardwareSerial.h>
 #include <FastLED.h>
-#include "dmx.h"
+#include <dmx.h>
 
 //---------------------------------
 
-#define BAUD_RATE           115200      // Of the serial port
-#define INTERNAL_LED_PIN    13          // For signaling what is being sent to the EV3
-#define EV3_PIN             19          // This pin is connected to the analog pin of EV3 for signaling if the clock should be advanced
-#define CLOCK_CHANNEL       1           // Channel that the DMX controller will use to control the clock -- This is PIN 13 on the ESP32 and DATA0 on the shield
-#define THRESHOLD           50          // Threshold of the value received to either decide to send HIGH or LOW to the EV3 brick
+#define BAUD_RATE             115200        // Of the serial port
+#define INTERNAL_LED_PIN      13            // For signaling what is being sent to the EV3
+#define EV3_PIN               19            // This pin is connected to the analog pin of EV3 for signaling if the clock should be advanced
+#define CLOCK_CHANNEL         1             // Channel that the DMX controller will use to control the clock -- This is PIN 13 on the ESP32 and DATA0 on the shield
+#define THRESHOLD             50            // Threshold of the value received to either decide to send HIGH or LOW to the EV3 brick
 
-#define RGB_LED_PIN         2
-#define COLOR_ORDER         GRB
-#define CHIPSET             WS2812
+#define RGB_LED_PIN           2
+#define COLOR_ORDER           GRB
+#define BRIGHTNESS            25
+#define CHIPSET               WS2812
 
 //---------------------------------
 
 DMX dmx;
 CRGB rgb_led[1];
 
-void setup() {  
+void setup()
+{
   Serial.begin(BAUD_RATE);
   pinMode(EV3_PIN, OUTPUT);
-  pinMode(INTERNAL_LED_PIN, OUTPUT);  
+  pinMode(INTERNAL_LED_PIN, OUTPUT);
   digitalWrite(EV3_PIN, LOW);
   digitalWrite(INTERNAL_LED_PIN, LOW);
 
   delay(2000);
 
   FastLED.addLeds<WS2812, RGB_LED_PIN, GRB>(rgb_led, 1).setCorrection(TypicalLEDStrip);
-  FastLED.setBrightness(25);
+  FastLED.setBrightness(BRIGHTNESS);
 
   Serial.printf("\n\n\n");
   Serial.printf("DMX512 to EV3 Bridge\n");
@@ -39,6 +41,9 @@ void setup() {
   Serial.printf("DMX Configured. Channel for clock is %d.\n", CLOCK_CHANNEL);
   Serial.printf("EV3 pin (%d) configured to LOW.\n\n", EV3_PIN);
 
+  rgb_led[0] = CRGB::Red;
+  FastLED.show();
+  delay(100);
 }
 
 int current_dmx = 0;
@@ -47,48 +52,53 @@ void loop()
 {
   int channels = 0;
 
-  if ((channels = dmx.update()) > 0) {
+  if ((channels = dmx.update()) > 0)
+  {
+    rgb_led[0] = CRGB::Green;
+    FastLED.show();
+    delay(100);
+    rgb_led[0] = CRGB::Blue;
+    FastLED.show();
+    delay(100);
+    rgb_led[0] = CRGB::Red;
+    FastLED.show();
+
     Serial.printf("\n");
     Serial.printf("Read %d channels from DMX512\n", channels);
     int from_dmx = dmx.read(CLOCK_CHANNEL);
     Serial.printf("Value of CLOCK channel (%d) = %d\n", CLOCK_CHANNEL, from_dmx);
 
-    if (from_dmx != current_dmx) {
+    if (from_dmx != current_dmx)
+    {
       current_dmx = from_dmx;
-      if (current_dmx < THRESHOLD) {
+      if (current_dmx < THRESHOLD)
+      {
         digitalWrite(EV3_PIN, LOW);
         digitalWrite(INTERNAL_LED_PIN, LOW);
         Serial.printf("Read new data from DMX (%d) -- Sending to EV3: LOW\n", current_dmx);
-      } else {
+      }
+      else
+      {
         digitalWrite(EV3_PIN, HIGH);
         digitalWrite(INTERNAL_LED_PIN, HIGH);
         Serial.printf("Read new data from DMX (%d) -- Sending to EV3: HIGH\n", current_dmx);
-      }    
+      }
     }
 
-
     Serial.printf("Current values of all channels:\n");
-    for (int i=0; i<channels; i++) {
-      if (i%32 == 0) {
+    for (int i = 0; i < channels; i++)
+    {
+      if (i % 32 == 0)
+      {
         Serial.printf("\n(%3i)  ", i);
       }
       Serial.printf("%4d", dmx.read(i));
     }
-  } else {
+  }
+  else
+  {
     Serial.printf("Couldn't get data from DMX512.\n");
   }
 
   Serial.printf("\n");
-
-  rgb_led[0] = CRGB::Red;
-  FastLED.show();
-  delay(100);
-
-  rgb_led[0] = CRGB::Green;
-  FastLED.show();
-  delay(100);
-
-  rgb_led[0] = CRGB::Blue;
-  FastLED.show();
-  delay(100);
 }
